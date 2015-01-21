6ebe92d4c03acda354af03962b179bdb76a7ee60
Ben Wu <wubin28@gmail.com>
2015-01-21 23:53:54 +0800

    Added an assertion for 5 in test the_player_should_say_buzz_if_the_number_is_a_multiple_of_5_but_not_a_multiple_of_15().

--- src/test/java/fizzbuzz/FizzBuzzPlayerTest.java
+++ src/test/java/fizzbuzz/FizzBuzzPlayerTest.java
@@ -1,5 +1,6 @@
 package fizzbuzz;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -12,13 +13,16 @@ import static org.junit.Assert.assertEquals;
  */
 public class FizzBuzzPlayerTest {
 
+    private FizzBuzzPlayer fizzBuzzPlayer;
+
+    @Before
+    public void initialize() {
+        fizzBuzzPlayer = new FizzBuzzPlayer();
+    }
+
     // TODO: the player should say the number if the number is normal (1, 2, 4, ..., 98)
     @Test
     public void the_player_should_say_the_number_if_the_number_is_normal() {
-        // Arrange
-        FizzBuzzPlayer fizzBuzzPlayer = new FizzBuzzPlayer();
-
-        // Act & Assert
         assertEquals("1", fizzBuzzPlayer.say(1));
         assertEquals("98", fizzBuzzPlayer.say(98));
     }
@@ -26,14 +30,15 @@ public class FizzBuzzPlayerTest {
     // TODO: the player should say fizz if the number is a multiple of 3 but not a multiple of 15 (3, 6, 9, 12, 18, ..., 99)
     @Test
     public void the_player_should_say_fizz_if_the_number_is_a_multiple_of_3_but_not_a_multiple_of_15() {
-        // Arrange
-        FizzBuzzPlayer fizzBuzzPlayer = new FizzBuzzPlayer();
-
-        // Act & Assert
         assertEquals("fizz", fizzBuzzPlayer.say(3));
         assertEquals("fizz", fizzBuzzPlayer.say(99));
     }
 
     // TODO: the player should say buzz if the number is a multiple of 5 but not a multiple of 15 (5, 10, 20, ..., 100)
+    @Test
+    public void the_player_should_say_buzz_if_the_number_is_a_multiple_of_5_but_not_a_multiple_of_15() {
+        assertEquals("buzz", fizzBuzzPlayer.say(5));
+    }
+
     // TODO: the player should say fizzbuzz if the number is both a multiple of 3 and a multiple of 5 (15, 30, 45, 60, 75, 90)
 }
