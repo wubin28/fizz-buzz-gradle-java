9c04f4ea13e54f46c2f4eac9f514ae0e37cdb0cd
Ben Wu <wubin28@gmail.com>
2015-01-26 23:47:57 +0800

    Wrote intention code in method Fizz.handle().

--- src/main/java/fizzbuzz/Fizz.java
+++ src/main/java/fizzbuzz/Fizz.java
@@ -3,9 +3,11 @@ package fizzbuzz;
 /**
  * Created by twer on 1/26/15.
  */
-public class Fizz implements FizzBuzzHandler {
+public class Fizz extends FizzBuzzHandler {
     @Override
     public String handle(int number) {
-        return null;
+        if (number % 3 == 0) return "fizz";
+        successor = new Buzz();
+        return successor.handle(number);
     }
 }
