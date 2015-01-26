d615a10cec64af20bfb0f5f24d0d6edf2a5db8b4
Ben Wu <wubin28@gmail.com>
2015-01-26 23:52:08 +0800

    Added class NormalNumber and its method handle().

---
+++ src/main/java/fizzbuzz/NormalNumber.java
@@ -0,0 +1,11 @@
+package fizzbuzz;
+
+/**
+ * Created by twer on 1/26/15.
+ */
+public class NormalNumber extends FizzBuzzHandler {
+    @Override
+    public String handle(int number) {
+        return String.valueOf(number);
+    }
+}
