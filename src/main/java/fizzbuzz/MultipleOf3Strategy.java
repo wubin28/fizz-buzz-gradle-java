21f6ca39f1c75243706a9edea134358ff07f0896
Ben Wu <wubin28@gmail.com>
2015-01-26 22:44:21 +0800

    Refactored to the strategy pattern for the test the_player_should_say_fizz_if_the_number_is_a_multiple_of_3_but_not_a_multiple_of_15().

---
+++ src/main/java/fizzbuzz/MultipleOf3Strategy.java
@@ -0,0 +1,11 @@
+package fizzbuzz;
+
+/**
+ * Created by twer on 1/26/15.
+ */
+public class MultipleOf3Strategy implements FizzBuzzStrategy {
+    @Override
+    public String say(int number) {
+        return "fizz";
+    }
+}
