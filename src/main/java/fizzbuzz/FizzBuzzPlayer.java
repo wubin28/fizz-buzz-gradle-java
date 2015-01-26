743cdf311cd2eb9f6205007a9970fa59d778d518
Ben Wu <wubin28@gmail.com>
2015-01-26 23:22:21 +0800

    Started to refactor to the chain of responsibility pattern in the method FizzBuzzPlayer.say().

--- src/main/java/fizzbuzz/FizzBuzzPlayer.java
+++ src/main/java/fizzbuzz/FizzBuzzPlayer.java
@@ -4,10 +4,12 @@ package fizzbuzz;
  * Created by twer on 1/21/15.
  */
 public class FizzBuzzPlayer {
-    public String say(int i) {
-        if (i % 15 == 0) return "fizzbuzz";
-        if (i % 3 == 0) return "fizz";
-        if (i % 5 == 0) return "buzz";
-        return String.valueOf(i);
+    public String say(int number) {
+        FizzBuzzHandler fizzBuzz = new FizzBuzz();
+        return fizzBuzz.handle(number);
+//        if (number % 15 == 0) return "fizzbuzz";
+//        if (number % 3 == 0) return "fizz";
+//        if (number % 5 == 0) return "buzz";
+//        return String.valueOf(number);
     }
 }
